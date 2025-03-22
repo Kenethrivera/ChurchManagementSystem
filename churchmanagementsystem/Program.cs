a7553f2e536bb64efd76a609683d6e6541efa3d9
Keneth Rivera <riverakenethjames@gmail.com>
2025-03-22 08:13:00 +0800

    Updated Methods
    Recode the methods. Apply methods with parameter, return, and no return. Fix some loops and username and password validation

--- churchmanagementsystem/Program.cs
+++ churchmanagementsystem/Program.cs
@@ -4,56 +4,69 @@ namespace churchmanagementsystem
 {
     internal class Program
     {
-        static string username = " ";
-        static string password = " ";
+        static List<string> usernames = new List<string>();
+        static List<string> passwords = new List<string>();
 
         static void Main(string[] args)
         {
-            Console.WriteLine("TEST PUSH FOR GITHUB");
             DisplayDashboard();
         }
 
         static void DisplayDashboard()
         {
-            int userChoice;
+            
             Console.WriteLine("\nWELCOME TO ADELINA CHRISTIAN CHURCH");
-            Console.WriteLine("------------------------------------");
+            DisplayLine();
             Console.WriteLine("[1] Register");
             Console.WriteLine("[2] Log in");
             Console.WriteLine("[3] Log in as an Admin (Leader)");
 
-            Console.Write("Enter your Choice: ");
-            userChoice = Convert.ToInt16(Console.ReadLine());
-
-            while (true)
-            {
+            int userChoice;
+            do { 
+                userChoice = UserChoice();
                 if (userChoice == 1)
                 {
                     RegisterUser();
-                    return;
                 }
                 else if (userChoice == 2)
                 {
                     SignInUser();
-                    return;
                 }
                 else if (userChoice == 3)
                 {
                     SignInLeader();
-                    return;
-                }
-                else
+                } else
                 {
-                    Console.WriteLine("Invalid input");
-                    return;
+                    Console.WriteLine("Error: User Input exceed 1-3 Options");
                 }
-            }
+            } while (userChoice > 3 || userChoice < 1);
+        }
+        
+        static int UserChoice()
+        {
+            Console.Write("Enter Choice: ");
+            int userChoice = Convert.ToInt16(Console.ReadLine());
+
+            return userChoice;
+        }
+
+        static void DisplayLine()
+        {
+            Console.WriteLine("------------------------------------");
+        }
+
+        static void HandlingUserPosition()
+        {
+            Console.Write("Enter Ministry: ");
+            string ministry = Console.ReadLine();
+            Console.Write("Enter Position: ");
+            string position = Console.ReadLine();
         }
 
         static void RegisterUser()
         {
             Console.WriteLine("\nREGISTRATION");
-            Console.WriteLine("-----------------------");
+            DisplayLine();
             Console.Write("First Name: ");
             string firstName = Console.ReadLine();
             Console.Write("Last Name: ");
@@ -62,34 +75,30 @@ namespace churchmanagementsystem
             int age = Convert.ToInt16(Console.ReadLine());
             Console.Write("Email Address: ");
             string emailAddress = Console.ReadLine();
+
             Console.Write("Are you an Officer/Admin? [1]Yes [2]No: ");
-            int positionChoices = Convert.ToInt16(Console.ReadLine());
-
-            if (positionChoices == 1)
+            int positionChoice = UserChoice();
+            
+            if (positionChoice == 1)
             {
-                Console.Write("Enter Ministry: ");
-                string ministry = Console.ReadLine();
-                Console.Write("Enter Position: ");
-                string position = Console.ReadLine();
-
+                HandlingUserPosition();
             }
-            else if (positionChoices == 2)
+            else if (positionChoice == 2)
             {
-
+                 
             }
             else
             {
-                Console.WriteLine("Invalid Input");
+                Console.WriteLine("Error: Input Exceeds Valid Range");
             }
 
             Console.Write("Create Username: ");
-            username = Console.ReadLine();
-
+            string username = Console.ReadLine();
 
             while (true)
             {
                 Console.Write("Create your Password: ");
-                password = Console.ReadLine();
+                string password = Console.ReadLine();
 
                 Console.Write("Re-Enter your Password: ");
                 string re_Enter_Password = Console.ReadLine();
@@ -97,12 +106,12 @@ namespace churchmanagementsystem
                 if (password != re_Enter_Password)
                 {
                     Console.WriteLine("Password does not Match. Try again");
-
                 }
                 else
                 {
-
                     Console.WriteLine("Registration Complete!!");
+                    usernames.Add(username);
+                    passwords.Add(password);
                     DisplayDashboard();
                     return;
 
@@ -110,173 +119,95 @@ namespace churchmanagementsystem
             }
         }
 
-        static void SignInUser()
+        static void ValidatingUsernameAndPassword()
         {
-            Console.WriteLine("\nACC USER LOGIN");
-            Console.WriteLine("-----------------");
-
-            string login_userName;
-            string login_password;
+            string loginUsername;
+            string loginPassword;
 
             int attempt = 0;
             int maxAttempt = 5;
 
             while (attempt < maxAttempt)
             {
-
                 Console.Write("Username: ");
-                login_userName = Console.ReadLine();
+                loginUsername = Console.ReadLine();
                 Console.Write("Password: ");
-                login_password = Console.ReadLine();
+                loginPassword = Console.ReadLine();
 
-                if (username == login_userName && password == login_password)
+                int index = usernames.IndexOf(loginUsername);
+                if (index != -1  && passwords[index] == loginPassword)
                 {
                     Console.WriteLine("Login Successfully");
-                    DisplayDashBoard();
+                    DisplayMinistryDashBoard();
                 }
 
                 attempt++;
-
-                if (attempt < maxAttempt)
-                {
-                    if (username != login_userName || password != login_password)
-                    {
-                        Console.WriteLine("No username or password found. Try Again");
-                        Console.WriteLine($"Attampt: {attempt} of {maxAttempt}");
-                    }
-                }
+                Console.WriteLine("No username or password found. Try Again");
+                Console.WriteLine($"Attempt: {attempt} of {maxAttempt}");
             }
             Console.WriteLine("Oops!!! You Reached Maximum Attempt. Try Again Later");
             Thread.Sleep(10000);
             DisplayDashboard();
+        }
+        static void SignInUser()
+        {
+            Console.WriteLine("\nACC USER LOGIN");
+            DisplayLine();
+            ValidatingUsernameAndPassword();
         }
 
         static void SignInLeader()
         {
             Console.WriteLine("\nACC LEADERS LOGIN");
-            Console.WriteLine("-----------------");
-
-            string login_userName;
-            string login_password;
-
-            int attempt = 0;
-            int maxAttempt = 5;
-
-            while (attempt < maxAttempt)
-            {
-                Console.Write("Ministry: ");
-                string ministry = Console.ReadLine();
-                Console.Write("Position: ");
-                string position = Console.ReadLine();
-                Console.Write("Username: ");
-                login_userName = Console.ReadLine();
-                Console.Write("Password: ");
-                login_password = Console.ReadLine();
-
-                if (username == login_userName && password == login_password)
-                {
-                    Console.WriteLine("Login Successfully");
-                    DisplayDashBoard();
-                    return;
-                }
-
-                attempt++;
-
-                if (attempt < maxAttempt)
-                {
-                    if (username != login_userName || password != login_password)
-                    {
-                        Console.WriteLine("No username or password found. Try Again");
-                        Console.WriteLine($"Attampt: {attempt} of {maxAttempt}");
-                    }
-                }
-            }
-            Console.WriteLine("Oops!!! You Reached Maximum Attempt. Try Again Later");
-            Thread.Sleep(10000);
-            DisplayDashboard();
+            DisplayLine();
+            HandlingUserPosition();
+            ValidatingUsernameAndPassword();
+            
         }
 
-        static void DisplayDashBoard()
+        static void DisplayMinistryDashBoard()
         {
             Console.WriteLine("\n Welcome to Adelina Christian Church");
             Console.WriteLine("Ministry: ");
 
-            string[] ministry = {"[1] Christian Education", "[2] Evagelism Ministrry",
-                    "[3] Worship Ministry", "[4] Descipleship Ministry",
-                    "[5] Admin", "[6] Prayer Ministry"};
+            string[] ministry = {"[1] Christian Education", "[2] Evangelism Ministry",
+                    "[3] Worship Ministry", "[4] Discipleship Ministry",
+                    "[5] Admin", "[6] Prayer Ministry", "[7] Exit"};
 
-            foreach (string choice in ministry)
+            foreach (string options in ministry)
             {
-                Console.WriteLine(choice);
+                Console.WriteLine(options);
             }
 
-            Console.Write("Enter Choice: ");
-            int userChoice = Convert.ToInt16(Console.ReadLine());
-
-            switch (userChoice)
-            {
-                case 1:
-                    christianEdDashboard();
-                    break;
-                case 2:
-                    evangelismDashboard();
-                    break;
-                case 3:
-                    worshipDashboard();
-                    break;
-                case 4:
-                    discipleshipDashboard();
-                    break;
-                case 5:
-                    aminDashboard();
-                    break;
-                case 6:
-                    prayerDashboard();
-                    break;
-                default:
-                    Console.WriteLine("Invalid Input. (Exceed 1-6)");
-                    DisplayDashBoard();
-                    break;
+            int userChoice = UserChoice();
+            string[] ministryName = {"Christian Education", "Evangelism Ministry", "Worship Ministry",
+                                    "Discipleship Ministry", "Admin", "Prayer Ministry"};
+            if(userChoice == 7){
+                DisplayDashboard();
             }
+            else if(userChoice >= 1 || userChoice <= 6 )
+            {
+                MinistryDashboard(ministryName[userChoice - 1]);
+            } else
+            {
+                Console.WriteLine("ERROR: Number exceeds the range");
+                DisplayMinistryDashBoard();
+            }
+            
         }
-        static void christianEdDashboard()
+        static void MinistryDashboard(string ministryName)
         {
             while (true)
             {
-                Console.WriteLine("\nChristian Education");
-                Console.WriteLine("------------------------");
+                Console.WriteLine($"\n {ministryName}");
+                DisplayLine();
                 Console.WriteLine("No data");
                 Console.WriteLine("[2] Return to User Dashboard");
-                Console.Write("Action: ");
-                int exit = Convert.ToInt16(Console.ReadLine());
 
-                if (exit == 2)
+                int userChoice = UserChoice();
+                if (userChoice == 2)
                 {
-                    DisplayDashBoard();
-                    return;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid Input");
-                }
-            }
-
-        }
-
-        static void evangelismDashboard()
-        {
-            while (true)
-            {
-                Console.WriteLine("\nEvangelism Ministry");
-                Console.WriteLine("------------------------");
-                Console.WriteLine("No data");
-                Console.WriteLine("[2] Return to User Dashboard");
-                Console.Write("Action: ");
-                int exit = Convert.ToInt16(Console.ReadLine());
-
-                if (exit == 2)
-                {
-                    DisplayDashBoard();
+                    DisplayMinistryDashBoard();
                     return;
                 }
                 else
@@ -285,97 +216,5 @@ namespace churchmanagementsystem
                 }
             }
         }
-        static void worshipDashboard()
-        {
-            while (true)
-            {
-                Console.WriteLine("\nWorship Ministry");
-                Console.WriteLine("------------------------");
-                Console.WriteLine("No data");
-                Console.WriteLine("[2] Return to User Dashboard");
-                Console.Write("Action: ");
-                int exit = Convert.ToInt16(Console.ReadLine());
-
-                if (exit == 2)
-                {
-                    DisplayDashBoard();
-                    return;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid Input");
-                }
-            }
-        }
-
-        static void discipleshipDashboard()
-        {
-            while (true)
-            {
-                Console.WriteLine("\nDescipleship Ministry");
-                Console.WriteLine("------------------------");
-                Console.WriteLine("No data");
-                Console.WriteLine("[2] Return to User Dashboard");
-                Console.Write("Action: ");
-                int exit = Convert.ToInt16(Console.ReadLine());
-
-                if (exit == 2)
-                {
-                    DisplayDashBoard();
-                    return;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid Input");
-                }
-            }
-        }
-
-        static void aminDashboard()
-        {
-            while (true)
-            {
-                Console.WriteLine("\nAdmin");
-                Console.WriteLine("------------------------");
-                Console.WriteLine("No data");
-                Console.WriteLine("[2] Return to User Dashboard");
-                Console.Write("Action: ");
-                int exit = Convert.ToInt16(Console.ReadLine());
-
-                if (exit == 2)
-                {
-                    DisplayDashBoard();
-                    return;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid Input");
-                }
-            }
-        }
-
-        static void prayerDashboard()
-        {
-            while (true)
-            {
-                Console.WriteLine("\nPrayer Ministry");
-                Console.WriteLine("------------------------");
-                Console.WriteLine("No data");
-                Console.WriteLine("[2] Return to User Dashboard");
-                Console.Write("Action: ");
-                int exit = Convert.ToInt16(Console.ReadLine());
-
-                if (exit == 2)
-                {
-                    DisplayDashBoard();
-                    return;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid Input");
-                }
-            }
-        }
-
     }
 }
